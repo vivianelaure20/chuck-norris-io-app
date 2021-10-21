45d8c5879bfc8ab8425091fe75b3c2159278b693
Felly Mwepu <fellymwepu@4-sure.net>
2021-10-21 20:12:37 +0200

    - Add App Center

--- app/src/main/java/io/fely/chucknorris_jokes/ui/MainActivity.kt
+++ app/src/main/java/io/fely/chucknorris_jokes/ui/MainActivity.kt
@@ -7,14 +7,25 @@ import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import io.fely.chucknorris_jokes.R
 import io.fely.chucknorris_jokes.utils.NetworkResource
+import com.microsoft.appcenter.crashes.Crashes
+
+import com.microsoft.appcenter.analytics.Analytics
+
+import com.microsoft.appcenter.AppCenter
+
+
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "d86a129d-2a95-41c5-a200-937f88ede39f",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     companion object {
